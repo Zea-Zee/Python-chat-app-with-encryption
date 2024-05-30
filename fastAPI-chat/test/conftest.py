fc634e9be6b671b8901a08ed988c546df162c7d7
Gleb Kuznetsov <g.kuznetsov2@g.nsu.ru>
2024-05-30 07:38:54 +0700

    comment fastapi app test

--- fastAPI-chat/test/conftest.py
+++ fastAPI-chat/test/conftest.py
@@ -1,25 +1,25 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeMeta, declarative_base
-from typing import AsyncGenerator
-from sqlalchemy import MetaData, NullPool
+# from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+# from sqlalchemy.orm import DeclarativeMeta, declarative_base
+# from typing import AsyncGenerator
+# from sqlalchemy import MetaData, NullPool
 
-from config import DB_HOST_TEST, DB_NAME_TEST, DB_PASS_TEST, DB_PORT_TEST, DB_USER_TEST
-from main import app
-
-
-DATABASE_URL_TEST = f"postgresql+asyncpg://{DB_USER_TEST}:{DB_PASS_TEST}@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_NAME_TEST}"
-BASE: DeclarativeMeta = declarative_base()
-
-metadata = MetaData()
-
-engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
-async_session_maker = async_sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
-metadata.bind = engine_test
+# from config import DB_HOST_TEST, DB_NAME_TEST, DB_PASS_TEST, DB_PORT_TEST, DB_USER_TEST
+# from main import app
 
 
-async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
-        yield session
+# DATABASE_URL_TEST = f"postgresql+asyncpg://{DB_USER_TEST}:{DB_PASS_TEST}@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_NAME_TEST}"
+# BASE: DeclarativeMeta = declarative_base()
+
+# metadata = MetaData()
+
+# engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
+# async_session_maker = async_sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
+# metadata.bind = engine_test
 
 
-app.dependency_overrides[get_async_session] = override_get_async_session
+# async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
+#     async with async_session_maker() as session:
+#         yield session
+
+
+# app.dependency_overrides[get_async_session] = override_get_async_session
